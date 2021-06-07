fca66dd5b1b335c5e1f9567f96be6c26104ca1de
Rustam <rustam.miko@gmail.com>
2021-06-07 11:30:37 +0300

    hw13 completed

--- app/src/main/java/com/rustamaliiev/sarmatapp/ui/movieDetails/FragmentMoviesDetails.kt
+++ app/src/main/java/com/rustamaliiev/sarmatapp/ui/movieDetails/FragmentMoviesDetails.kt
@@ -7,6 +7,7 @@ import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
